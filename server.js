882c92d5804c54f0829863ef7e1322267c025c4d
Nikko Pisciotti <t.bird.13@hotmail.com>
2017-03-13 14:10:02 -0700

    built out server file

---
+++ server.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const express = require('express');
+const debug = require('debug')('quiver:server');
+const Promise = require('bluebird');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const morgan = require('morgan');
+
+dotenv.load();
+
+const PORT = process.env.PORT;
+const app = express();
+mongoose.connect(process.env.MONGODB_URI);
+
+app.use(cors());
+app.use(morgan('dev'));
+
+app.listen(PORT, () => {
+  debug(`Port up: ${PORT}`);
+});
