784c410423e4926c8fa94dae9f4d069edcb3a913
william-skelton <william.f.skelton@outlook.com>
2017-03-14 13:13:36 -0700

    added venue-route.js

--- route/user-route.js
+++ route/user-route.js
@@ -7,9 +7,9 @@ const basicAuth = require('../lib/basic-auth-middleware.js');
 
 const User = require('../model/user.js');
 
-const authRouter = module.exports = Router();
+const userRouter = module.exports = Router();
 
-authRouter.post('/api/signup', jsonParser, function(req, res, next) {
+userRouter.post('/api/signup', jsonParser, function(req, res, next) {
   debug('POST: /api/signup');
 
   let password = req.body.password;
@@ -24,7 +24,7 @@ authRouter.post('/api/signup', jsonParser, function(req, res, next) {
   .catch(next);
 });
 
-authRouter.get('/api/signin', basicAuth, function(req, res, next) {
+userRouter.get('/api/signin', basicAuth, function(req, res, next) {
   debug('GET: /api/signin');
 
   User.findOne( { username: req.auth.username } )
