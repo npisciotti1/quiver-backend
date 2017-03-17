9759041eb4210a7f1aa7e2c6f2f5996aaf652828
Shiv <shiv.unadkat@gmail.com>
2017-03-16 19:02:18 -0700

    a whole hell of a lot of venue tests, added the put route for it and that is tested too

--- test/venue-route-test.js
+++ test/venue-route-test.js
@@ -25,7 +25,7 @@ const exampleVenue = {
   address: '123 funktown'
 };
 
-describe('Venue Route Test', function() {
+describe('THE VENUE ROUTES MODULE --', function() {
   afterEach( done => {
     Promise.all([
       User.remove({}),
@@ -35,8 +35,7 @@ describe('Venue Route Test', function() {
     .catch(done);
   });
 
-  describe('POST /api/venue', function() {
-
+  describe('for POST routes in VENUE -', function() {
     before( done => {
       new User(exampleUser)
       .generatePasswordHash(exampleUser.password)
@@ -87,7 +86,204 @@ describe('Venue Route Test', function() {
     });
 
     describe('With an invalid route and valid body', () => {
+      it('should return a 404 error', done => {
+        request.post(`${url}/api/wrongplacedunce`)
+        .send(exampleVenue)
+        .set({
+          Authorization: `Bearer ${this.tempToken}`
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(404);
+          done();
+        });
+      });
+    });
 
+    describe('if the user is not authorized', () => {
+      it('should return a 401 error', done => {
+        request.post(`${url}/api/venue`)
+        .send(exampleVenue)
+        .set({
+          Authorization: `NOT TODAY YOUNG ONE`
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(401);
+          done();
+        })
+      });
+    });
+  });
+
+  describe('for GET routes in VENUE -', function() {
+    before( done => {
+      new User(exampleUser)
+      .generatePasswordHash(exampleUser.password)
+      .then( user => user.save())
+      .then( user => {
+        this.tempUser = user;
+        return user.generateToken();
+      })
+      .then( token => {
+        this.tempToken = token;
+        done();
+      })
+      .catch(done);
+    });
+    before( done => {
+      exampleVenue.userID = this.tempUser._id.toString();
+      new Venue(exampleVenue).save()
+      .then( venue => {
+        this.tempVenue = venue;
+        done();
+      })
+      .catch(done);
+    });
+
+    after( done => {
+      delete exampleVenue.userID;
+      done();
+    });
+
+    it('should successfully return a venue', done => {
+      request.get(`${url}/api/venue/${this.tempVenue._id}`)
+      .set({
+        Authorization: `Bearer ${this.tempToken}`
+      })
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.status).to.equal(200);
+        expect(res.body.name).to.equal(exampleVenue.name);
+        expect(res.body.desciption).to.equal(exampleVenue.desciption);
+        expect(res.body.userID).to.equal(this.tempUser._id.toString());
+        done();
+      });
+    });
+    it('should return an error for wrong endpoint', done => {
+      request.get(`${url}/api/venue/somewrongshit`)
+      .set({
+        Authorization: `Bearer ${this.tempToken}`
+      })
+      .end((err, res) => {
+        expect(res.status).to.equal(404)
+        done();
+      });
+    });
+
+    it('should return an error for not being authorized', done=> {
+      request.get(`${url}/api/venue/${this.tempVenue._id}`)
+      .set({
+        Authorization: `not going through my dude`
+      })
+      .end((err, res) => {
+        expect(res.status).to.equal(401)
+        done();
+      });
+    });
+  });
+
+  describe('for PUT routes in VENUE -', function() {
+    before( done => {
+      new User(exampleUser)
+      .generatePasswordHash(exampleUser.password)
+      .then( user => user.save())
+      .then( user => {
+        this.tempUser = user;
+        return user.generateToken();
+      })
+      .then( token => {
+        this.tempToken = token;
+        done();
+      })
+      .catch(done);
+    });
+
+    before( done => {
+      exampleVenue.userID = this.tempUser._id.toString();
+      new Venue(exampleVenue).save()
+      .then( venue => {
+        this.tempVenue = venue;
+        done();
+      })
+      .catch(done);
+    });
+
+    after( done => {
+      if (this.tempUser) {
+        Promise.all([
+          User.remove({}),
+          Venue.remove({})
+        ])
+        .then( () => done())
+        .catch(done);
+        return;
+      }
+      done();
+    });
+
+    it('will return an updated venue', done => {
+      let newVenue = { name: 'ONTRACK', address: 'BEASTMODE'};
+      request.put(`${url}/api/venue/${this.tempVenue._id}`)
+      .set({
+        Authorization: `Bearer ${this.tempToken}`
+      })
+      .send(newVenue)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.status).to.equal(200);
+        expect(res.body.userID).to.equal(this.tempUser._id.toString())
+        expect(res.body.name).to.equal(newVenue.name);
+        expect(res.body.address).to.equal(newVenue.address);
+        done();
+      });
+    });
+
+    it('did not SEND the updated venue correctly', done => {
+      let newVenue = { name: 'BADBOY', address: 'IS A STRAIGHT TITAN'};
+      request.put(`${url}/api/venue/${this.tempVenue._id}`)
+      .set({
+        Authorization: `Bearer ${this.tempToken}`
+      })
+      .end((err, res) => {
+        expect(res.body).to.equal(null);
+        done();
+      });
+    });
+
+    it('sent the updated venue to the WRONG ENDPOINT', done => {
+      let newVenue = { name: 'OHSHIT', address: 'THIS MIGHT BE FORREAL'};
+      request.put(`${url}/api/venue/wrongplacesexy`)
+      .set({
+        Authorization: `Bearer ${this.tempToken}`
+      })
+      .end((err, res) => {
+        expect(res.status).to.equal(404);
+        done();
+      });
+    });
+
+    // it('did not actually UPDATE the venue', done => {
+    //   let newVenue = { name: 'FLOW STATE', address: 'FEELS GREAT'};
+    //   request.get(`${url}/api/venue/${this.tempVenue._id}`)
+    //   .set({
+    //     Authorization: `Bearer ${this.tempToken}`
+    //   })
+    //   .send(exampleVenue)
+    //   .end((err, res) => {
+    //     if (err) return done(err);
+    //     expect(res.body.name).to.equal(exampleVenue.name);
+    //     exepct(res.body.address).to.equal(exampleVenue.address);
+    //     expect(res.body.userID).to.equal(this.tempUser._id.toString());
+    //     done();
+    //   });
+    // });
+    it('was an unauthorized request', done => {
+      let newVenue = { name: 'more', address: 'and MORE'};
+      request.put(`${url}/api/venue/${this.tempVenue.id}`)
+      .send(newVenue)
+      .end((err, res) => {
+        expect(res.status).to.equal(401);
+        done();
+      });
     });
   });
 });
