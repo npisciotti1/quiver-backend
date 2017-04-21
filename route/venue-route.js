ec97ab132016dcfca49ddbdc04de7cb25294dec0
shivprogrammer <shiv.unadkat@gmail.com>
2017-04-21 10:55:35 -0700

    added back in the commented out get route of all venues

--- route/venue-route.js
+++ route/venue-route.js
@@ -34,13 +34,13 @@ venueRouter.get('/api/venue/:id', bearerAuth, function(req, res, next) {
 });
 
 
-// venueRouter.get('/api/venue', function(req, res, next) {
-//   debug('GET: /api/venue');
-//
-//   Venue.find({})
-//   .then( venue => res.json(venue))
-//   .catch( () => next(createError(404, 'not found')));
-// });
+venueRouter.get('/api/venue', function(req, res, next) {
+  debug('GET: /api/venue');
+
+  Venue.find({})
+  .then( venue => res.json(venue))
+  .catch( () => next(createError(404, 'not found')));
+});
 
 venueRouter.put('/api/venue/:id', bearerAuth, jsonParser, function(req, res, next) {
   debug('PUT: /api/venue/:id');
