ed186122fbe5893199771093076e03439e53b38e
william-skelton <william.f.skelton@outlook.com>
2017-05-08 09:42:05 -0700

    changed venue schema to have phone number and hours

--- model/venue.js
+++ model/venue.js
@@ -6,6 +6,8 @@ const Schema = mongoose.Schema;
 const venueSchema = Schema({
   name: { type: String, required: true},
   address: { type: String, required: true},
+  phone: { type: String, required: true},
+  hours: { type: String, required: true},
   profPic: { type: Schema.Types.ObjectId, ref: 'pic' },
   pics: [{ type: Schema.Types.ObjectId, ref: 'pic' }],
   userID: { type: Schema.Types.ObjectId, required: true, unique: true },
